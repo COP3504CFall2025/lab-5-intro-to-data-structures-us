314f871fb9f1630549c6259fd566a88017fecdc3
ksokolovUF <ksokolov@ufl.edu>
2025-11-29 22:32:13 -0500

    Finish linked list

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -1,48 +1,156 @@
 #pragma once
+#include <assert.h>
 #include <iostream>
-using namespace std;
 
+template <typename T> class LinkedList {
+  struct Node {
+    T data;
+    Node *next;
+    Node *prev;
+  };
 
-
-template <typename T>
-class LinkedList {
 public:
-	// Behaviors
-	void printForward() const;
-	void printReverse() const;
+  // Behaviors
+  void printForward() const {
+    Node *current = head;
+    while (current != nullptr) {
+      std::cout << current->data << std::endl;
+      current = current->next;
+    }
+  }
+  void printReverse() const {
+    Node *current = tail;
+    while (current != nullptr) {
+      std::cout << current->data << std::endl;
+      current = current->prev;
+    }
+  }
 
-	// Accessors
-	[[nodiscard]] unsigned int getCount() const;
-	Node* getHead();
-	const Node* getHead() const;
-	Node* getTail();
-	const Node* getTail() const;
+  // Accessors
+  [[nodiscard]] unsigned int getCount() const { return count; }
+  Node *getHead() { return head; }
+  const Node *getHead() const { return head; }
+  Node *getTail() { return tail; }
+  const Node *getTail() const { return tail; }
 
-	// Insertion
-	void addHead(const T& data);
-	void addTail(const T& data);
+  // Insertion
+  void addHead(const T &data) {
+    Node *h = new Node;
+    h->data = data;
+    h->next = head;
+    h->prev = nullptr;
+    if (head)
+      head->prev = h;
+    else
+      tail = h;
+    head = h;
+    count++;
+  }
+  void addTail(const T &data) {
+    Node *t = new Node;
+    t->data = data;
+    t->next = nullptr;
+    t->prev = tail;
+    if (tail)
+      tail->next = t;
+    else
+      head = t;
+    tail = t;
+    count++;
+  }
 
-	// Removal
-	bool removeHead();
-	bool removeTail();
-	void Clear();
+  // Removal
+  bool removeHead() {
+    if (count <= 1) {
+      return false;
+    }
+    head = head->next;
+    delete head->prev;
+    head->prev = nullptr;
+    count--;
+    return true;
+  }
+  bool removeTail() {
+    if (count <= 1) {
+      return false;
+    }
+    tail = tail->prev;
+    delete tail->next;
+    tail->next = nullptr;
+    count--;
+    return true;
+  }
+  void Clear() {
+    while (count > 1) {
+      removeHead();
+    }
+    if (head)
+      count--;
+    assert(head == tail);
+    delete head;
+    head = nullptr;
+    tail = nullptr;
+    assert(count == 0);
+  }
 
-	// Operators
-	LinkedList<T>& operator=(LinkedList<T>&& other) noexcept;
-	LinkedList<T>& operator=(const LinkedList<T>& rhs);
+  // Operators
+  LinkedList<T> &operator=(LinkedList<T> &&other) noexcept {
+    if (this == &other) {
+      return this;
+    }
+    Clear();
+    head = other.head;
+    tail = other.tail;
+    count = other.count;
 
-	// Construction/Destruction
-	LinkedList();
-	LinkedList(const LinkedList<T>& list);
-	LinkedList(LinkedList<T>&& other) noexcept;
-	~LinkedList();
+    other.head = nullptr;
+    other.tail = nullptr;
+    other.count = 0;
+
+    return *this;
+  }
+
+  LinkedList<T> &operator=(const LinkedList<T> &rhs) {
+    if (this == &rhs) {
+      return *this;
+    }
+    Clear();
+    Node *current = rhs.head;
+    while (current != nullptr) {
+      addTail(current->data);
+      current = current->next;
+    }
+    return *this;
+  }
+
+  // Construction/Destruction
+  LinkedList() {
+    head = nullptr;
+    tail = nullptr;
+    count = 0;
+  }
+
+  LinkedList(const LinkedList<T> &list) {
+    Node *current = list.head;
+    while (current != nullptr) {
+      addTail(current->data);
+      current = current->next;
+    }
+  }
+  LinkedList(LinkedList<T> &&other) noexcept {
+    head = other.head;
+    tail = other.tail;
+    count = other.count;
+
+    other.head = nullptr;
+    other.tail = nullptr;
+    other.count = 0;
+  }
+  ~LinkedList() { Clear(); }
 
 private:
-	// Stores pointers to first and last nodes and count
-	Node* head;
-	Node* tail;
-	unsigned int count;
-
+  // Stores pointers to first and last nodes and count
+  Node *head;
+  Node *tail;
+  unsigned int count;
 };
-
-
