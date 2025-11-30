587ec8bbbd15d9df4cdc1fc8a88484f198eb6525
ksokolovUF <ksokolov@ufl.edu>
2025-11-30 13:43:19 -0500

    fuck LL queue and stack

--- LLQ.hpp
+++ LLQ.hpp
@@ -2,27 +2,46 @@
 
 #include "Interfaces.hpp"
 #include "LinkedList.hpp"
-#include <stdlib.h>
 #include <stdexcept>
+#include <stdlib.h>
 
-template <typename T>
-class LLQ : public QueueInterface<T> {
+template <typename T> class LLQ : public QueueInterface<T> {
 private:
-    LinkedList<T> list;
+  LinkedList<T> list;
+
 public:
-    // Constructor
-    LLQ();
+  // Constructor
+  LLQ() = default;
+  ~LLQ() = default;
 
-    // Insertion
-    void enqueue(const T& item) override;
+  // Insertion
+  void enqueue(const T &item) override { list.addTail(item); }
 
-    // Deletion
-    T dequeue() override;
+  // Deletion
+  T dequeue() override {
+    if (list.getCount > 0) {
+      T val = list.getHead()->data;
+      if (list.getCount() == 1)
+        list.Clear();
+      else
+        list.removeHead();
+      return val;
+    } else {
+      throw "empty linked list";
+    }
+  }
 
-    // Access
-    T peek() const override;
+  // Access
+  T peek() const override {
+    if (list.getCount() > 0)
+      list.getHead()->data;
+    else
+      throw "empty linked list";
+  }
 
-    // Getter
-    std::size_t getSize() const noexcept override;
+  // Getter
+  std::size_t getSize() const noexcept override { return list.getCount(); }
 
+  void PrintForward() { list.printForward(); }
+  void PrintReverse() { list.printReverse(); }
 };
--- LLS.hpp
+++ LLS.hpp
@@ -2,26 +2,46 @@
 
 #include "Interfaces.hpp"
 #include "LinkedList.hpp"
-#include <stdlib.h>
 #include <stdexcept>
+#include <stdlib.h>
 
-template <typename T>
-class LLS : public StackInterface<T> {
+template <typename T> class LLS : public StackInterface<T> {
 private:
-    LinkedList<T> list;
+  LinkedList<T> list;
+
 public:
-    // Constructor
-    LLS();
+  // Constructor
+  LLS() = default;
+  ~LLS() override = default;
 
-    // Insertion
-    void push(const T& item) override;
+  // Insertion
+  void push(const T &item) override { list.addHead(item); }
 
-    // Deletion
-    T pop() override;
+  // Deletion
+  T pop() override {
+    if (list.getCount() > 0) {
+      T val = list.getHead()->data;
+      if (list.getCount() == 1)
+        list.Clear();
+      else
+        list.removeHead();
+      return val;
+    } else {
+      throw "empty array";
+    }
+  }
 
-    // Access
-    T peek() const override;
+  // Access
+  T peek() const override {
+    if (list.getCount() > 0)
+      return list.getTail()->data;
+    else
+      throw "empty array";
+  }
 
-    //Getters
-    std::size_t getSize() const noexcept override;
+  // Getters
+  std::size_t getSize() const noexcept override { return list.getCount(); }
+
+  void PrintForward() { list.printForward(); }
+  void PrintReverse() { list.printReverse(); }
 };
